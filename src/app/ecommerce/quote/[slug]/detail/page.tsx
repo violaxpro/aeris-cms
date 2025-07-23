76df7afe515c1e63522d14cf37b72038416d45f0
Viola Yosevi <viola.yosevi19@gmail.com>
2025-07-23 16:22:45 +0700

    feat : add page detail quote

---
+++ src/app/ecommerce/quote/[slug]/detail/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import DetailQuote from '@/features/pages/sales/quote/DetailQuote'
+import { Params } from '@/plugins/types'
+import { quoteDummyData } from '@/plugins/types/sales-type';
+
+export default async function DetailQuotePage(props: { params: Params }) {
+    const params = await props.params;
+    const slug = params.slug;
+    let data
+    try {
+
+        const dataByid = quoteDummyData.find((item) => {
+            return item.id === Number(slug)
+        })
+        data = dataByid
+    } catch (error) {
+
+    }
+
+    return (
+        <div>
+            <DetailQuote slug={slug} data={data} />
+        </div>
+    )
+}
+
+
